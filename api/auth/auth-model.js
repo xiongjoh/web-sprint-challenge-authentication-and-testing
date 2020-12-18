ace5b67a3183b54d3f097f87a5f7e8e35cfe10e0
unknown <xiongjoh@gmail.com>
2020-12-18 09:42:04 -0600

    add auth-model

---
+++ api/auth/auth-model.js
@@ -0,0 +1,21 @@
+const db = require('../../data/dbConfig')
+
+module.exports = {
+    async register(user) {
+        try {
+            const [id] = db('users').insert(user, 'id')
+            return db('users').where({id}).first()
+        } catch (err){
+            return `Oops, something went wrong: ${err.message}`
+        }
+    },
+    getUsers(){
+        return db('users').select('id', 'username', 'password')
+    },
+    findBy(filter){
+        return db('users').where(filter).first()
+    },
+    findById(id){
+        return db('users').where({id}).first()
+    }
+}
