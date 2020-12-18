acb9dda0ca7317632f8dcdd07dab192712a6f5cf
unknown <xiongjoh@gmail.com>
2020-12-18 09:28:27 -0600

    add restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../../config/secret");
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.status(401).json({ message: `token required` });
+  } else {
+    jwt.verify(token, jwtSecret, (err, decode) => {
+      if (err) {
+        res
+          .status(401)
+          .json({
+            message: `token invalid`,
+          });
+      } else {
+        req.decodedToken = decode;
+        next();
+      }
+    });
+  }
+
   /*
     IMPLEMENT
 
